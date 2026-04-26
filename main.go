56dc7ae26e3c07ed71991c33d5bbfd582a916f9b
BenKelenski <ben.kelenski@gmail.com>
2026-04-25 22:15:30 -0400

    center output with loading icon

--- main.go
+++ main.go
@@ -3,35 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"charm.land/bubbles/v2/list"
+	"charm.land/bubbles/v2/spinner"
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
 
 const listWidth = 40
 const listHeight = 20
 
 func initialModel() model {
+	s := spinner.New(spinner.WithSpinner(spinner.Dot))
+	return model{
+		spinner: s,
+		loading: true,
+	}
+}
+
+func fetchRooms() tea.Msg {
 	rooms, err := GetRooms()
-	if err != nil {
-		fmt.Printf("error while getting rooms: %s\n", err)
-		os.Exit(1)
-	}
-
-	items := make([]list.Item, len(rooms))
-	for i, r := range rooms {
-		items[i] = r
-	}
-
-	delegate := list.NewDefaultDelegate()
-	l := list.New(items, delegate, listWidth, listHeight)
-	l.Title = "Rooms"
-
-	return model{list: l}
+	return roomsLoadedMsg{rooms: rooms, err: err}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(m.spinner.Tick, fetchRooms)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -40,6 +37,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if msg.String() == "q" || msg.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		m.windowWidth = msg.Width
+		m.windowHeight = msg.Height
+	case roomsLoadedMsg:
+		if msg.err != nil {
+			fmt.Printf("error while getting rooms: %s\n", msg.err)
+			return m, tea.Quit
+		}
+		items := make([]list.Item, len(msg.rooms))
+		for i, r := range msg.rooms {
+			items[i] = r
+		}
+		delegate := list.NewDefaultDelegate()
+		l := list.New(items, delegate, listWidth, listHeight)
+		l.Title = "Rooms"
+		m.list = l
+		m.loading = false
+		return m, nil
+	}
+
+	if m.loading {
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
 	}
 
 	var cmd tea.Cmd
@@ -48,7 +69,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
-	return tea.NewView(m.list.View())
+	var content string
+	if m.loading {
+		content = fmt.Sprintf("%s Loading rooms…", m.spinner.View())
+	} else {
+		content = m.list.View()
+	}
+	centered := lipgloss.Place(m.windowWidth, m.windowHeight,
+		lipgloss.Center, lipgloss.Center,
+		content,
+	)
+	return tea.View{Content: centered, AltScreen: true}
 }
 
 func main() {
--- model.go
+++ model.go
@@ -1,7 +1,19 @@
 package main
 
-import "charm.land/bubbles/v2/list"
+import (
+	"charm.land/bubbles/v2/list"
+	"charm.land/bubbles/v2/spinner"
+)
+
+type roomsLoadedMsg struct {
+	rooms []Room
+	err   error
+}
 
 type model struct {
-	list list.Model
+	list         list.Model
+	spinner      spinner.Model
+	loading      bool
+	windowWidth  int
+	windowHeight int
 }
